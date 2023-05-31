59598c8df977f2e6d37ee7ff6d9112536fd3f020
sirconceptz <mateusz.hermanowicz@mypantry.eu>
2023-05-31 14:43:16 +0200

    Add bluetooth events broadcast receiver

---
+++ app/src/main/java/com/hermanowicz/wirelesswhisper/receivers/BluetoothBroadcastReceiver.kt
@@ -0,0 +1,76 @@
+package com.hermanowicz.wirelesswhisper.receivers
+
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
+
+@AndroidEntryPoint
+class BluetoothBroadcastReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context?, intent: Intent) {
+        val action = intent.action
+        val state: Int
+        val bluetoothDevice: BluetoothDevice?
+
+        Timber.d("BroadcastActions", "Action " + action + "received")
+
+        when (action) {
+            BluetoothAdapter.ACTION_STATE_CHANGED -> {
+                state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, -1)
+                when (state) {
+                    BluetoothAdapter.STATE_OFF -> {
+                        Toast.makeText(context, "Bluetooth is off", Toast.LENGTH_SHORT).show()
+                        Timber.d("BroadcastActions", "Bluetooth is off")
+                    }
+
+                    BluetoothAdapter.STATE_TURNING_OFF -> {
+                        Toast.makeText(context, "Bluetooth is turning off", Toast.LENGTH_SHORT)
+                            .show()
+                        Timber.d("BroadcastActions", "Bluetooth is turning off")
+                    }
+
+                    BluetoothAdapter.STATE_ON -> {
+                        Timber.d("BroadcastActions", "Bluetooth is on")
+                    }
+                }
+            }
+
+            BluetoothDevice.ACTION_ACL_CONNECTED -> {
+                bluetoothDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
+                try {
+                    Toast.makeText(
+                        context,
+                        "Connected to " + bluetoothDevice!!.name,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    Timber.d("BroadcastActions", "Connected to " + bluetoothDevice.name)
+                } catch (e: SecurityException) {
+                    Timber.e("BroadcastActions", "Error: $e")
+                } catch (e: Exception) {
+                    Timber.e("BroadcastActions", "Error: $e")
+                }
+            }
+
+            BluetoothDevice.ACTION_ACL_DISCONNECTED -> {
+                bluetoothDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
+                try {
+                    Toast.makeText(
+                        context,
+                        "Disconnected from " + bluetoothDevice!!.name,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    Timber.d("BroadcastActions", "Connected to " + bluetoothDevice.name)
+                } catch (e: SecurityException) {
+                    Timber.e("BroadcastActions", "Error: $e")
+                } catch (e: Exception) {
+                    Timber.e("BroadcastActions", "Error: $e")
+                }
+            }
+        }
+    }
+}
